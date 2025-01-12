caac911585f0cbb784b05300acc29cc20de311e9
benjamin-mahmood <benmahmood@icloud.com>
2025-01-12 14:10:10 +0100

    show previous saves as list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let count = 0;
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 function increment() {
   count++;
@@ -7,5 +8,7 @@ function increment() {
 }
 
 function save() {
+  let savedCount = count + ' - ';
+  saveEl.innerText += savedCount;
   console.log(count);
 }
